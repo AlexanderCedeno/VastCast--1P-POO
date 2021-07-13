1e231d228419f23634da9d90c9b060dbda9c95e9
Walter Mix <Walter Mix@DESKTOP-9LTC3D4>
2021-07-13 15:15:14 -0500

    Modificaciones programa principal

---
+++ Documents/NetBeansProjects/SistemaNotify/src/sistemanotify/SistemaNotify.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemanotify;
+
+import java.util.function.Predicate;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.*;
+//Importar desde el paquete clase
+import Classes.Observation;
+import Classes.PopUp;
+import Classes.PopUpObs;
+import Classes.Propiedad;
+import Classes.User;
+import Classes.Device;
+//Importar desde el paquete Settings
+import Settings.Archivo;
+import Settings.FilterCollector;
+/**
+ *
+ * @author Kevin Cedeño
+ * @coauthor Kevin Valle
+ */
+public class SistemaNotify {
+
+    public static List<Observation> observations = new ArrayList<>();
+    public static List<Device> devices = new ArrayList<>();
+    public static List<Propiedad> propiedades=new ArrayList<>();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Archivo a = new Archivo();
+        //a.leerTxt("C:\\Users\\Walter Mix\\Documents\\NetBeansProjects\\SistemaNotify\\src\\Settings\\iot_telemetry_data_new.csv");
+         a.leerTxt("C:\\Users\\Walter Mix\\Documents\\NetBeansProjects\\SistemaNotify\\src\\Settings\\muestra.txt");
+        List<String> datas = a.getData();
+        FilterCollector filter=new FilterCollector();
+        
+        //Crea una lista con todos los id_dispositivos para recorrer las observaciones
+        List<String> idDev=new ArrayList<>();
+        idDev=filter.collectorId(datas);
+
+        
+
+        //Filtra las opciones dentro del csv
+         for (String dataID : idDev) {
+             Device deviceN= new Device(dataID);
+             devices.add(deviceN);
+            datas.stream().filter(id -> id.split(",")[1].equals(dataID)).map(id -> id.split(",")[2]).forEach(id -> {
+                
+               propiedades.add(new Propiedad("CO"));
+                observations.add(new Observation(id));
+
+            });
+            a.IterO(datas, dataID);
+
+       }
+
+// System.out.println(observations.size());
+        for (Observation ob : observations) {
+            System.out.println(ob.getValue());
+        }
+  
+        for (Device pr:devices){
+            System.out.println(pr.getDevice());
+        }
+        for (Propiedad prop:propiedades){
+            System.out.println(prop.getNombre());
+        }
+}
+}
