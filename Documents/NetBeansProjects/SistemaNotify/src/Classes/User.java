fe58a3267abd529fb03c6e16489bcb8a236e747b
AlexanderCedeno <kevcduma@espol.edu.ec>
2021-07-16 14:08:59 -0500

    Estoy completando la clase de Usuario. Psdt: No modificar nada aquí!!

--- Documents/NetBeansProjects/SistemaNotify/src/Classes/User.java
+++ Documents/NetBeansProjects/SistemaNotify/src/Classes/User.java
@@ -1,16 +1,38 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * @author Kevin Valle
  * @author Kevin Cedeno
  * @author Gustavo Castro
  */
 public class User {
-
+    private List<Device> devices;
+    private List<PopUpObs> xObs;
+    private List<PopUpDev> xDev;
     private String id_User;
 
     public User(String id) {
         id_User = id;
+        devices=new ArrayList<>();
+        xObs= new ArrayList<>();
+        xDev=new ArrayList<>();
     }
-
+    
+    
+    public String getIDUser(){
+        return id_User;
+    }
+    
+    public List<Device> getDevicesRoll(){
+        return devices;
+    }
+    
+    public List<PopUpObs> getPopUpObs(){
+        return xObs;
+    }
+    public List<PopUpDev> getPopUpDev(){
+        return xDev;
+    }
 }
