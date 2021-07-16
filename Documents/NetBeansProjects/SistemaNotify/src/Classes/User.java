2a4d5d4e5290b13bef7d6c98c3e86c98b1596c46
AlexanderCedeno <kevcduma@espol.edu.ec>
2021-07-16 16:22:54 -0500

    Inice con el codigo para el metodo createPopUp, aún falta(No editar), se puede probar

--- Documents/NetBeansProjects/SistemaNotify/src/Classes/User.java
+++ Documents/NetBeansProjects/SistemaNotify/src/Classes/User.java
@@ -2,12 +2,15 @@ package Classes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author Kevin Valle
  * @author Kevin Cedeno
  * @author Gustavo Castro
  */
 public class User {
+
     private List<Device> devices;
     private List<PopUpObs> xObs;
     private List<PopUpDev> xDev;
@@ -15,24 +18,75 @@ public class User {
 
     public User(String id) {
         id_User = id;
-        devices=new ArrayList<>();
-        xObs= new ArrayList<>();
-        xDev=new ArrayList<>();
+        devices = new ArrayList<>();
+        xObs = new ArrayList<>();
+        xDev = new ArrayList<>();
     }
-    
-    
-    public String getIDUser(){
+
+    public String getIDUser() {
         return id_User;
     }
-    
-    public List<Device> getDevicesRoll(){
+
+    public List<Device> getDevicesRoll() {
         return devices;
     }
-    
-    public List<PopUpObs> getPopUpObs(){
+
+    public List<PopUpObs> getPopUpObs() {
         return xObs;
     }
-    public List<PopUpDev> getPopUpDev(){
+
+    public List<PopUpDev> getPopUpDev() {
         return xDev;
+    }
+
+    public void enrollDev(Device d) {
+        devices.add(d);
+    }
+
+    public void createPopUp(String l) {
+        Scanner sc = new Scanner(System.in);
+        if (!l.equals("light") && !l.equals("motion")) {
+            String opcion = "";
+            while (!opcion.equals("1") || !opcion.equals("2") || !opcion.equals("3")) {
+                System.out.println("Elija para que rangos desea configurar:");
+                System.out.println("1.-Para rangos: ValorObservacion > vMax");
+                System.out.println("2.Para rangos entre: vMin>ValorObservacion<vMax");
+                System.out.println("3.-Para rangos: ValorObservacion < vMin");
+
+                System.out.print("Opcion:");
+                opcion = sc.nextLine();
+                String label = "";
+                double vMax = 0;
+                double vMin = 0;
+                switch (opcion) {
+
+                    case "1":
+                        System.out.print("Escriba una etiqueta(Ejmplo: Peligro): ");
+                        label = sc.nextLine();
+                        System.out.print("Escriba el valor máximo: ");
+                        vMax = sc.nextInt();
+                        xObs.add(new PopUpObs(l, label, vMax, opcion));
+                        break;
+                    case "2":
+                        System.out.print("Escriba una etiqueta(Ejmplo: Moderado): ");
+                        label = sc.nextLine();
+                        System.out.print("Escriba el valor máximo: ");
+                        vMax = sc.nextDouble();
+                        System.out.print("Escriba el valor mínimo: ");
+                        vMin = sc.nextDouble();
+                        xObs.add(new PopUpObs(l, label, vMax, vMin, opcion));
+                        break;
+                    case "3":
+                        System.out.print("Escriba una etiqueta(Ejmplo: Controlado): ");
+                        label = sc.nextLine();
+                        System.out.print("Escriba el valor mínimo: ");
+                        vMin = sc.nextInt();
+                        xObs.add(new PopUpObs(l, label, vMin, opcion));
+                        break;
+
+                }
+            }
+
+        }
     }
 }
