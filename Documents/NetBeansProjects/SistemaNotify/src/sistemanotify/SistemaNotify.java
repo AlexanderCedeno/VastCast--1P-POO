9d79b6307bbf385d2fca7b31a8721311ad0760b1
AlexanderCedeno <kevcduma@espol.edu.ec>
2021-07-16 20:23:11 -0500

    Creando el main y sus opciones.. posiblemente haya que crear otra clase

--- Documents/NetBeansProjects/SistemaNotify/src/sistemanotify/SistemaNotify.java
+++ Documents/NetBeansProjects/SistemaNotify/src/sistemanotify/SistemaNotify.java
@@ -5,6 +5,7 @@
  */
 package sistemanotify;
 
+import java.text.ParseException;
 import java.util.function.Predicate;
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -14,6 +15,8 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Iterator;
 import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 //Importar desde el paquete clase
 import Classes.Observation;
 import Classes.PopUp;
@@ -26,6 +29,9 @@ import Classes.PopUpDev;
 import Settings.Archivo;
 import Settings.FilterCollector;
 import Settings.DevGenerator;
+import Settings.Writer;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 /**
  *
@@ -39,11 +45,14 @@ public class SistemaNotify {
     public static List<Device> devices = new ArrayList<>();
     public static List<Propiedad> propiedades = new ArrayList<>();
     public static List<String> cabecera = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
+    public static Scanner scan = new Scanner(System.in);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         Archivo a = new Archivo();
         //a.leerTxt(ruta);iot_telemetry_data_new.csv
         //String ruta="C:\\Users\\kevin\\Documents\\NetBeansProjects\\VastCast--1P-POO\\Documents\\NetBeansProjects\\SistemaNotify\\src\\Settings\\muestra.txt";//Ruta para prueba compañeros
@@ -51,8 +60,6 @@ public class SistemaNotify {
         a.leerTxt(ruta);
         List<String> datos = a.getData();
         FilterCollector filter = new FilterCollector();
-        
-        
 
         //Crea una lista con todos los id_dispositivos para recorrer las observaciones
         DevGenerator dg = new DevGenerator();
@@ -65,7 +72,7 @@ public class SistemaNotify {
         /* for (Observation ob : observations) {
             System.out.println(ob.getValue());
         }*/
-        /*List<PopUpObs> xObs=new ArrayList<>();
+ /*List<PopUpObs> xObs=new ArrayList<>();
         xObs.add(new PopUpObs("temp", "Peligro", 22.0, "1"));
         xObs.add(new PopUpObs("light", "Peligro", "t"));
         List<PopUpDev> dev=new ArrayList<>();
@@ -82,11 +89,135 @@ public class SistemaNotify {
                 
             }
         }*/
-        
-        User user1=new User("JuanFranciscoAlelio");
+ /*User user1=new User("JuanFranciscoAlelio");
         user1.createPopUp("motion");
         for (PopUpObs po:user1.getPopUpObs()){
         System.out.println(po.getLabel());
-        }        
+        }*/
+        registerUser();
+        System.out.println(users);
+        programmNotify();
+
     }
+
+    public static void rangeDates() throws ParseException {
+        /*SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
+        Date initDate=format.parse("10/05/2021");
+        Date endDate=format.parse("22/05/2021");
+        Date fechaA= new Date();
+        Writer generator= new Writer();
+        LocalDate startLocalDate = initDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate endLocalDate = endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                System.out.println(startLocalDate);
+        List<LocalDate> dates=generator.getRangeDates(startLocalDate,endLocalDate);
+        System.out.println(dates);
+         */
+
+    }
+
+    /* public static void menu(){
+        String opcion="";  
+    while(!opcion.equals("4")){
+    System.out.println("Sistema de notificación");
+    System.out.println("1.-Registrar Usuario");
+    System.out.println("2.-Iniciar Sesion");
+    System.out.println("4.-Salir");
+    
+    System.out.print("Opcion:");
+    opcion = scan.nextLine();
+    switch(opcion){
+      case "1":
+        depositar();
+        break;
+      case "2":
+        retirar();
+        break;
+        case "3":
+        retirar();
+        break;
+
+    }
+    }
+    }*/
+
+    public static Device inputDevice() {
+        System.out.print("Ingrese su dispositivo :");
+        String device = scan.nextLine();
+
+        for (Device dev : devices) {
+            if (dev.getDevice().equals(device)) {
+                return dev;
+            }
+        }
+        return null;
+    }
+
+    public static String inputProperty() {
+        System.out.print("Ingrese etiqueta de Propiedad. ('co','humidity', 'light', 'lpg', 'motion', 'smoke', 'temp )");
+        String label = scan.nextLine();
+
+        for (String p : cabecera) {
+            if (p.equals(label)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static void registerUser() {
+        System.out.print("Ingrese id de usuario :");
+        String userID = scan.nextLine();
+
+        users.add(new User(userID));
+    }
+
+    public static void logIn() {
+        String opcion = "";
+        while (!opcion.equals("4")) {
+            System.out.println("Sistema de notificación");
+            System.out.println("1.-Programar notificacion");
+            System.out.println("2.-Generar notificaciones");
+            System.out.println("3.-Desactivar notificaciones.");
+
+            System.out.print("Opcion:");
+            opcion = scan.nextLine();
+            switch (opcion) {
+                case "1":
+                    /*User user1=new User("JuanFranciscoAlelio");
+        user1.createPopUp("motion");
+        for (PopUpObs po:user1.getPopUpObs()){
+        System.out.println(po.getLabel());
+        }*/
+                    //depositar();
+                    break;
+                case "2":
+                    //retirar();
+                    break;
+                case "3":
+                    // retirar();
+                    break;
+
+            }
+        }
+    }
+
+    public static void programmNotify() {
+
+        for (User u : users) {
+            String evaluator = "";
+            while (!evaluator.equals("salida")) {
+                String label = inputProperty();
+                if (label != null) {
+                    u.createPopUp(label);
+                    for (PopUpObs po : u.getPopUpObs()) {
+                        System.out.println(po.getLabel());
+                    }
+                    evaluator="salida";
+                } else {evaluator="no";
+                    System.out.println("No existe la propiedad. Vuelva a intentarlo ");
+                }
+            }
+        }
+    }
+
 }
